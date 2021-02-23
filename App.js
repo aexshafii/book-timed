2150b375ad500307a0aecf08539874110e36661c
spacewaves <alexandre.shafii@gmail.com>
2021-02-23 14:58:16 -0800

    feat: ability to sign up and login

---
+++ App.js
@@ -0,0 +1,83 @@
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import * as firebase from "firebase";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LandingScreen from "./components/auth/Landing";
+import RegisterScreen from "./components/auth/Register";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCGy97Pshyl6fiqOJwUMtWLsT0neYlgYsI",
+  authDomain: "book-time-8e655.firebaseapp.com",
+  projectId: "book-time-8e655",
+  storageBucket: "book-time-8e655.appspot.com",
+  messagingSenderId: "480801466816",
+  appId: "1:480801466816:web:c4ab5536380825a1e40025",
+  measurementId: "G-QLE6BBCMRJ",
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const Stack = createStackNavigator();
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+    };
+  }
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        });
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        });
+      }
+    });
+  }
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if (!loaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <Text>Loading</Text>
+        </View>
+      );
+    }
+    if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen
+              name="Landing"
+              component={LandingScreen}
+            ></Stack.Screen>
+            <Stack.Screen
+              name="Register"
+              component={RegisterScreen}
+            ></Stack.Screen>
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+    return (
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <Text>USer is logged in</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,2 @@
+# book-timed
+# book-timed
