27a95a8e783acdb465e870ac8a066d7f707fa40c
spacewaves <alexandre.shafii@gmail.com>
2021-02-23 18:01:22 -0800

    feat: current user information retrived and displayed on main app page

--- components/Main.js
+++ components/Main.js
@@ -9,15 +9,22 @@ export class Main extends Component {
     this.props.fetchUser();
   }
   render() {
+    const { currentUser } = this.props;
+    console.log(currentUser);
+    if (currentUser == undefined) {
+      return <View></View>;
+    }
     return (
       <View style={{ flex: 1, justifyContent: "center" }}>
-        <Text>USer is logged in</Text>
+        <Text>{currentUser.name} is logged in</Text>
       </View>
     );
   }
 }
-
+const mapStateToProps = (store) => ({
+  currentUser: store.userState.currentUser,
+});
 const mapDispatchProps = (dispatch) =>
   bindActionCreators({ fetchUser }, dispatch);
 
-export default connect(null, mapDispatchProps)(Main);
+export default connect(mapStateToProps, mapDispatchProps)(Main);
