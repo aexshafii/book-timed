5921d895fc53c377913779f6765f87e1139385a0
spacewaves <alexandre.shafii@gmail.com>
2021-02-25 15:03:41 -0800

    feat: ability to log out

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { View, Text } from "react-native";
+import firebase from "firebase";
 
 export default function Profile() {
+  const onLogout = () => {
+    firebase.auth().signOut();
+  };
   return (
     <View>
       <Text>Profile</Text>
+      <button title="Logout" onClick={() => onLogout()}>
+        Logout
+      </button>
     </View>
   );
 }
