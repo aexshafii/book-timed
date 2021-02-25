bb7bd5a45b39b5248d3529620a392af17f3158a0
spacewaves <alexandre.shafii@gmail.com>
2021-02-25 15:25:37 -0800

    feat: adapted logout button for react native, now also works on mobile version

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import firebase from "firebase";
 
 export default function Profile() {
@@ -9,9 +9,7 @@ export default function Profile() {
   return (
     <View>
       <Text>Profile</Text>
-      <button title="Logout" onClick={() => onLogout()}>
-        Logout
-      </button>
+      <Button title="Logout" onPress={() => onLogout()}></Button>
     </View>
   );
 }
