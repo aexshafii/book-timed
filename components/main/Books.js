aabc7f14000f95ddd37da9ddb03ea731bed947a2
spacewaves <alexandre.shafii@gmail.com>
2021-03-05 17:05:07 -0800

    style: added plus button icon

--- components/main/Books.js
+++ components/main/Books.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Button } from "react-native-elements";
+
 import Timer from "../Timer/Timer";
 import Search from "../search/search";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Icon from "react-native-vector-icons/FontAwesome";
+
 export default function BooksScreen() {
   return (
     <View>
+      <Button
+        icon={<MaterialCommunityIcons name="plus" color="black" size={26} />}
+      />
+
       <Search></Search>
     </View>
   );
